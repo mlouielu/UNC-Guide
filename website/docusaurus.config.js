b96c1fb11ce4e0ef055d5a79cd159b9df7e24ec5
Louie Lu <git@louie.lu>
2025-08-25 22:48:15 -0400

    Fix search

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -48,6 +48,7 @@ const config = {
       /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
       ({
         hashed: true,
+		docsRouteBasePath: "/",
         language: ["en", "zh"],
         highlightSearchTermsOnTargetPage: true,
         explicitSearchResultPath: true,
