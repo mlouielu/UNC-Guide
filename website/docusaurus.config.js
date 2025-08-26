05e512a657f0a9ff819c58758831e33b8468ba9e
Louie Lu <git@louie.lu>
2025-08-25 22:08:47 -0400

    Add last update author and time

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -65,7 +65,9 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/mlouielu/UNC-Guide/tree/main/website',
+          'https://github.com/mlouielu/UNC-Guide/tree/main/website',
+		  showLastUpdateAuthor: true,
+		  showLastUpdateTime: true,
         },
 		blog: false,
         theme: {
