f716dbacc88707ffe4c35022c71d8663e5ad1d78
Louie Lu <git@louie.lu>
2026-07-23 11:52:53 -0400

    Add sitemap

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -79,6 +79,18 @@ const config = {
 		  remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
         },
+		sitemap: {
+		  lastmod: 'datetime',
+		  changefreq: 'weekly',
+		  priority: 0.5,
+		  ignorePatterns: ['/tags/**'],
+		  filename: 'sitemap.xml',
+          createSitemapItems: async (params) => {
+            const {defaultCreateSitemapItems, ...rest} = params;
+            const items = await defaultCreateSitemapItems(rest);
+            return items.filter((item) => !item.url.includes('/page/'));
+          },
+		},
 		blog: false,
         theme: {
           customCss: './src/css/custom.css',
